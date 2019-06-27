77a02673d799fb6ed841c88e990f19b91cc1363b
Anthony ESTEBE <anthony@mesg.com>
2019-06-27 14:43:08 +0700

    simplify app

--- index.js
+++ index.js
@@ -1,33 +1,36 @@
 const mesg = require('mesg-js').application(require('./config')())
 
-const callSmartContract = (method, inputs) => mesg.executeTask({
-  serviceID: 'evm-contract',
-  taskKey: 'execute',
-  inputData: JSON.stringify({
-    method,
-    privateKey: process.env.ALICE_PRIVATE_KEY,
-    inputs
-  })
-})
-
-const challengeData = async utxo_pos => {
-  const res = await mesg.executeTaskAndWaitResult({
-    serviceID: 'plasma-watcher',
-    taskKey: 'getChallengeData',
-    inputData: JSON.stringify({ utxo_pos })
-  })
-  return JSON.parse(res.outputData)
-}
-
 mesg.listenEvent({ serviceID: 'plasma-watcher', eventFilter: 'invalid_exit' })
   .on('data', async event => {
-    const data = JSON.parse(event.eventData)
-    const challenge = await challengeData(data.utxo_pos)
-    console.log(challenge)
-    await callSmartContract('challengeStandardExit', [
-      challenge.exit_id, // _standardExitId
-      challenge.txbytes, // _challengeTx
-      challenge.input_index, // _inputIndex
-      challenge.sig, // _challengeTxSig
-    ])
+    const invalidExit = JSON.parse(event.eventData)
+
+    console.log('Receiving an invalid exit', invalidExit)
+
+    const challengeResponse = await mesg.executeTaskAndWaitResult({
+      serviceID: 'plasma-watcher',
+      taskKey: 'getChallengeData',
+      inputData: JSON.stringify({
+        utxo_pos: invalidExit.utxo_pos
+      })
+    })
+    const challenge = JSON.parse(challengeResponse.outputData)
+
+    console.log('Challenging the invalid exit', challenge)
+
+    const challengeExitResponse = await mesg.executeTaskAndWaitResult({
+      serviceID: 'evm-contract',
+      taskKey: 'execute',
+      inputData: JSON.stringify({
+        method: 'challengeStandardExit',
+        privateKey: process.env.BOB_PRIVATE_KEY,
+        inputs: [
+          challenge.exit_id,
+          challenge.txbytes,
+          challenge.input_index,
+          challenge.sig,
+        ]
+      })
+    })
+
+    console.log('Challenge sent', JSON.parse(challengeExitResponse.outputData))
   })
